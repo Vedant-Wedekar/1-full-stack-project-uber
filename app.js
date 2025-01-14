07cf19289f0b5e76c9156a616c03fa90f420b004
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-01-14 22:37:09 +0530

    main file

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookiesparser = require('cookie-parser');
 const connectToDB = require('./DB/db');
-
-
-
+const userRoutes = require('./routes/user.routes');
 connectToDB();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+// Routes
+
+
+
 
 
 
@@ -18,4 +24,5 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.use('/users', userRoutes);
 module.exports = app;
