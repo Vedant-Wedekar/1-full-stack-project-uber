12dc2f5a0320ee6fbe1debdccdafd205aa78c855
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-01-14 22:37:50 +0530

    data base file

--- DB/db.js
+++ DB/db.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 function connectToDB() {
-    mongoose.connect(process.env.DB_connect, {
-       
-    })
+    mongoose.connect(process.env.DB_connect)
     .then(() => console.log('Connected to MongoDB'))
     .catch(error => console.error('Error connecting to MongoDB:', error));
 }
