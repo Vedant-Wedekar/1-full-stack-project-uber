008bbaad37ac4017cbc3258c8395010e15a3aae7
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-01-16 20:11:43 +0530

    user route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
+const userController = require('../controller/user.controller');
 
-
-
+router.post('/register', [
+    body('email').isEmail().withMessage('Email is invalid'),
+    body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long'),
+    body('fullname.firstName').isLength({ min: 2 }).withMessage('First name must be at least 2 characters long'),
+] , userController.registerUser);  // Validate the input data
 
 
 
