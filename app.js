c75bc0221bc80195367cbc8e8313cf3baa30cd40
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-01-17 09:41:48 +0530

    main file

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const cookiesparser = require('cookie-parser');
+const cookieParser = require('cookie-parser'); // Fixed variable name
 const connectToDB = require('./DB/db');
 const userRoutes = require('./routes/user.routes');
+
+// Connect to the database
 connectToDB();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 // Routes
-
-
-
-
-
-
-
-
 app.get('/', (req, res) => {    
     res.send('Hello World!');
 });
 
 app.use('/users', userRoutes);
+
 module.exports = app;
