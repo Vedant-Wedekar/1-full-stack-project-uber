4dc6d62a76c3a3a57e5cfaa3d57d12e092726ef0
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-02-10 07:30:27 +0530

    home page starts

---
+++ uber-clone/FRONTEND/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='h-screen w-full'>
+       <div className="div"> 
+        <h2>Get Started with lel Cooper </h2>
+        <button className='btn '>Continue</button>
+      </div>
+       
+    </div>
+  )
+}
+
+export default Home
