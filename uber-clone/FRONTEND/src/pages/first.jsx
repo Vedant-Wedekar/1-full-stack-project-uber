1bdb527e547f1e1bfb34a48ac22fd1b3fa48b758
VEDANT WEDEKAR <vedantwedekar@gmail.com>
2025-02-10 07:02:14 +0000

    own created

---
+++ uber-clone/FRONTEND/src/pages/first.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+const first = () => {
+  return (
+    <div className='h-screen w-full'>
+       <div className=""> 
+
+  <div className="firstcurve">
+    <div className="light_circ h-20 w-20 rounded"></div>
+<div className="gird justify-center align-center ">
+    <h5> lee Cooper </h5>
+    <h2>Get There</h2>
+</div>
+<div className=""></div>
+  </div>
+      </div>
+       
+    </div>
+  )
+}
+
+export default first 
