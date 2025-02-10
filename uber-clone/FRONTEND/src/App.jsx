6d01658022f67ab921b4bdda83db85ca80f5b46c
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-02-10 07:31:04 +0530

    all routes are connected to the specified App file

--- uber-clone/FRONTEND/src/App.jsx
+++ uber-clone/FRONTEND/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import {Route, Routes } from 'react-router-dom'
+import CaptainLogin from './pages/CaptainLogin'
+import Home from './pages/Home'
+import Userlogin from './pages/Userlogin'
+import CaptainSignUp from './pages/CaptainSignUp'
+import UserSignup from './pages/UserSignup'
 const App = () => {
   return (
     <div>
-       <h1 class="flex justify-center text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <Routes> 
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Userlogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/captain-signup" element={<CaptainSignUp />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+      </Routes>
+      
     </div>
   )
 }
