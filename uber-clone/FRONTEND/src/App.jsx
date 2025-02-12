eb14760f962a0e4fa1ddf5e5552c1be06ef4feeb
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-02-12 10:33:43 +0530

    accept the fact

--- uber-clone/FRONTEND/src/App.jsx
+++ uber-clone/FRONTEND/src/App.jsx
@@ -5,15 +5,21 @@ import Home from './pages/Home'
 import Userlogin from './pages/Userlogin'
 import CaptainSignUp from './pages/CaptainSignUp'
 import UserSignup from './pages/UserSignup'
+import First from './pages/First'
+import Opt from './pages/Opt'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 const App = () => {
   return (
     <div>
       <Routes> 
-        <Route path="/" element={<Home />} />
+        <Route path="/f" element={<Home />} />
         <Route path="/login" element={<Userlogin />} />
         <Route path="/signup" element={<UserSignup />} />
         <Route path="/captain-signup" element={<CaptainSignUp />} />
         <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/opt" element={<Opt />} />
+       <Route path="/" element={<First />} />
       </Routes>
       
     </div>
